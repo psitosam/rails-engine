659a04dc87133cf009621cc764a62ea871048918
Alex Psitos <apsitos08@gmail.com>
2022-05-18 12:53:29 -0400

    Feat: test to find all merchants matching search term name passes

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,11 +1,20 @@
 class Api::V1::SearchesController < ApplicationController
 
-  def find
+  def find #should this be find merchant
     merchant = Merchant.where("name ILIKE ?","%#{params[:name]}%").first
     if merchant.nil?
       render json: { data: { message: "No merchant found" } }
     else
       render  json: MerchantSerializer.new(merchant)
-    end 
+    end
+  end
+
+  def find_all_merchants
+    merchants = Merchant.where("name ILIKE ?","%#{params[:name]}%").order(:name)
+    if merchants.nil?
+      render json: { data: { message: "No merchants found" } }
+    else
+      render json: MerchantSerializer.new(merchants)
+    end
   end
 end
