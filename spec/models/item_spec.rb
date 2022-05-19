a0bf7de81db19e84a86b04eb69c66b9139d0c24b
Alex Psitos <apsitos08@gmail.com>
2022-05-19 18:21:32 -0400

    All functionality complete

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to(:merchant) }
   end
+
+# there is only one class method for Item, the search_all method. It is tested implicitly in the items_requests_spec.rb file at line 135.
 end
